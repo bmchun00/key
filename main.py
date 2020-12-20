efbe421fe990e8c9fa0d6addec08439001299af9
bmchun00 <bmchun00@uos.ac.kr>
2020-12-21 03:20:31 +0900

    alpha

--- main.py
+++ main.py
@@ -23,15 +23,26 @@ class MyApp(QWidget):
         tabs.addTab(stat, 'Stat')
         tabs.addTab(changelog, 'ChangeLog')
 
-
         vbox = QVBoxLayout()
         vbox.addWidget(tabs)
+
+        tabs.currentChanged.connect(self.tcEvent,tabs.currentIndex())
+        print(tabs.currentIndex())
 
         self.setLayout(vbox)
 
         self.setWindowTitle('key(가제) 1.00a')
         self.setGeometry(300, 300, 500, 300)
         self.show()
+
+    def tcEvent(self,changedInd):
+        if changedInd == 1 or changedInd == 2:
+            num, ok = QInputDialog.getInt(self, '문장 수', '수행할 문장 수를 입력해 주세요.')
+            if ok:
+                print(num)
+
+
+
 
 class MainTap(QWidget):
     def __init__(self):
@@ -57,15 +68,25 @@ class MainTap(QWidget):
 
         self.setLayout(vbox)
 
+
 class TextTap(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
     def initUI(self):
-        self.Tapclick()
+        title = QLabel("기본 텍스트 입니다.", self)
+        title.setAlignment(Qt.AlignCenter)
+        get = QLineEdit()
+        get.setAlignment(Qt.AlignCenter)
+        pbar = QProgressBar(self)
+        pbar.setAlignment(Qt.AlignVCenter)
+        vbox = QVBoxLayout()
+        vbox.addWidget(title)
+        vbox.addWidget(get)
+        vbox.addWidget(pbar)
 
-    def Tapclick(self):
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your name:')
+        self.setLayout(vbox)
+
 
 
 if __name__ == '__main__':
