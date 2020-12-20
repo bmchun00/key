c28875fa200682419eff5351d999ace5b2a1c9f7
bmchun00 <bmchun00@uos.ac.kr>
2020-12-21 04:02:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5.QtCore import Qt
 
+typenum = 0
+class communicate(QObject):
+    getnum = pyqtSignal()
 
 class MyApp(QWidget):
 
@@ -10,37 +14,45 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        main = MainTap()
-        text = TextTap()
-        news = QWidget()
-        stat = QWidget()
-        changelog = QWidget()
+        self.main = MainTap()
+        self.text = TextTap()
+        self.news = QWidget()
+        self.stat = QWidget()
+        self.changelog = QWidget()
 
-        tabs = QTabWidget()
-        tabs.addTab(main, 'Main')
-        tabs.addTab(text, 'Internal Text')
-        tabs.addTab(news, 'News')
-        tabs.addTab(stat, 'Stat')
-        tabs.addTab(changelog, 'ChangeLog')
+        self.tabs = QTabWidget()
+        self.tabs.addTab(self.main, 'Main')
+        self.tabs.addTab(self.text, 'Internal Text')
+        self.tabs.addTab(self.news, 'News')
+        self.tabs.addTab(self.stat, 'Stat')
+        self.tabs.addTab(self.changelog, 'ChangeLog')
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(tabs)
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.tabs)
 
-        tabs.currentChanged.connect(self.tcEvent,tabs.currentIndex())
-        print(tabs.currentIndex())
+        self.tabs.currentChanged.connect(self.tcEvent)
+        print(self.tabs.currentIndex())
 
-        self.setLayout(vbox)
+        self.setLayout(self.vbox)
 
-        self.setWindowTitle('key(가제) 1.00a')
+        self.setWindowTitle('key(가제) 1.01a')
         self.setGeometry(300, 300, 500, 300)
         self.show()
 
-    def tcEvent(self,changedInd):
+    def tcEvent(self):
+        changedInd = self.tabs.currentIndex()
         if changedInd == 1 or changedInd == 2:
             num, ok = QInputDialog.getInt(self, '문장 수', '수행할 문장 수를 입력해 주세요.')
             if ok:
-                print(num)
+                self.text.pbar.setMaximum(num)
+                self.text.pbar.setValue(0)
+            else:
+                self.tabs.setCurrentIndex(0)
 
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Return:
+            val = self.text.pbar.value()
+            self.text.pbar.setValue(val+1)
 
 
 
@@ -73,23 +85,24 @@ class TextTap(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+
     def initUI(self):
-        title = QLabel("기본 텍스트 입니다.", self)
-        title.setAlignment(Qt.AlignCenter)
-        get = QLineEdit()
-        get.setAlignment(Qt.AlignCenter)
-        pbar = QProgressBar(self)
-        pbar.setAlignment(Qt.AlignVCenter)
-        vbox = QVBoxLayout()
-        vbox.addWidget(title)
-        vbox.addWidget(get)
-        vbox.addWidget(pbar)
-
-        self.setLayout(vbox)
-
+        self.title = QLabel("기본 텍스트 입니다.", self)
+        self.title.setAlignment(Qt.AlignCenter)
+        self.get = QLineEdit(self)
+        self.get.setAlignment(Qt.AlignCenter)
+        self.pbar = QProgressBar(self)
+        self.pbar.setAlignment(Qt.AlignVCenter)
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.title)
+        self.vbox.addWidget(self.get)
+        self.vbox.addWidget(self.pbar)
+        self.setLayout(self.vbox)
 
 
 if __name__ == '__main__':
+    typenum = 0
     app = QApplication(sys.argv)
     ex = MyApp()
+    print(typenum)
     sys.exit(app.exec_())
